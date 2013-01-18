8ad009ec54800e81b8474178f64f08e07f4abdf8
Robert Schulze <robert@dotless.de>
2013-01-18 11:51:15 +0000

    initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RablRailsTesting::Application.config.secret_token = '4db761096b0645d275883f5ab5a13b03d035b1c5e91873eb2bb526bf8fa7fca88ca19fcf87f0471ed781c412c89467e6606b252b8c1dd6f190b5307e9675c358'
